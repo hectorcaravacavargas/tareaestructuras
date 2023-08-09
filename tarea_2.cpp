cbdbdf800fcbb33c8815d1fbca8362c138e36663
Hector Alonso Caravaca Vargas <hcaravacavargas@gmail.com>
2023-08-08 18:45:01 -0600

    add menu funcition

--- tarea_2.cpp
+++ tarea_2.cpp
@@ -100,12 +100,84 @@ void fillStringMatriz(){
 // arrayChar[n]
 // stringArray[i][j]
 
+void fillMatrizWithChar(){
+    int maxCharRow = -1, maxCharColumn = -1, maxCharCount = 0;
+    for (int row = 0; row < n; ++row) {
+        for (int column = 0; column < m; ++column) {
+
+            int charCount = 0;
+            for (char c : stringArray[row][column]) {
+                for (int i = 0; i < n; ++i) {
+                    if (c == arrayChar[i]) {
+                        charCount++;
+                        break;
+                    }
+                }
+            }
+
+            if (charCount > maxCharCount) {
+                maxCharCount = charCount;
+                maxCharRow = row;
+                maxCharColumn = column;
+            }
+        }
+    }
+
+    // Imprimir la respuesta
+    if (maxCharRow != -1 && maxCharColumn != -1) {
+        std::cout << "La celda " << maxCharRow << "," << maxCharColumn << " tiene " << maxCharCount
+        << " char de los que aparecen en el arreglo." << std::endl;
+    } else {
+        std::cout << "No se encontró ninguna celda con caracteres coincidentes." << std::endl;
+    }
+}
+
+// INSTRUCTION #6;
+void mostrarMenu(){
+    int opcion;
+    bool salir = false;
+
+    do{
+        std::cout << "\n\t.:MENU:." << std::endl;
+        std::cout << "1. Mostrar el array de numeros random." << std::endl;
+        std::cout << "2. Mostrar y solicitar el array de caracteres." << std::endl;
+        std::cout << "3. Mostrar la matriz de numeros random." << std::endl;
+        std::cout << "4. Solicitar y mostrar la matriz de string." << std::endl;
+        std::cout << "5. Mostrar la matriz de string y comparar con el array de char." << std::endl;
+        std::cout << "6. Salir." << std::endl;
+        std::cout << "Digite la opcion que desea: ";
+        std::cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            fillArrayRandomNumber();
+            break;
+        case 2:
+            fillArrayChar();
+            break;
+        case 3:
+            fillMatrizWithArray();
+            break;
+        case 4:
+            fillStringMatriz();
+            break;
+        case 5:
+            fillMatrizWithChar();  // Usar la función para mostrar, no llenar
+            break;
+        case 6:
+            std::cout << "Saliendo del programa..." << std::endl;
+            salir = true;
+            break;
+        default:
+            break;
+        }
+    } while (!salir);
+}
+
 // MAIN FUNCTION;
 int main() {
     srand(time(0));
-    fillArrayRandomNumber();
-    fillMatrizWithArray();
-    fillArrayChar();
-    fillStringMatriz();
+    mostrarMenu();
     return 0;
 }
