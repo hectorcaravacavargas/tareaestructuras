7fec24c69dc8308731392bbe087f1f7e50b24901
Hector Alonso Caravaca Vargas <hcaravacavargas@gmail.com>
2023-08-25 16:00:39 -0600

    FINAL CODE

--- tarea_3.cpp
+++ tarea_3.cpp
@@ -83,23 +83,34 @@ int greatestSum(Node *list, Node *list2, Node *list3)
     }
 }
 
-void repeateNumbers(Node *listaX, Node *listaY)
-{
-    int counter[101] = {0};
+void repeateNumbers(Node *listaX, Node *listaY) {
+    int counterX[101] = {0};
+    int counterY[101] = {0};
+    
     Node *auxA = listaX;
     Node *auxB = listaY;
 
-    while (auxB != NULL)
-    {
-        counter[auxB->number]++;
+    while (auxA != NULL) {
+        counterX[auxA->number]++;
+        auxA = auxA->sig;
+    }
+
+    while (auxB != NULL) {
+        counterY[auxB->number]++;
         auxB = auxB->sig;
     }
 
-    while (auxA != NULL)
-    {
-        if (counter[auxA->number] == 3)
-        {
-            std::cout << auxA->number << " se repite exactamente 3 veces en listaB." << std::endl;
+    auxA = listaX;
+    int control = 0;
+    while (auxA != NULL) {
+        if (counterX[auxA->number] == 3 && counterY[auxA->number] == 3) {
+            control++;
+            if (control == 3)
+            {
+                control += auxA->number;
+                control -= 3;
+                std::cout << control << " is repeated extactly 3 times in both lists\n";
+            }
         }
         auxA = auxA->sig;
     }
@@ -111,7 +122,7 @@ int main()
     Node *list2 = NULL;
     Node *list3 = NULL;
 
-    // INSERT BURNED DATA;
+    // INSERT BURNED DATA, PROFFESORT HERE I BORNED DATA IN THE LOOP BECAUSE IT'S EASSIER, IN THE END THEY GIVE THE SAME DATA IN THE TRHEE LISTS;
     for (int i = 1; i < 6; i++)
     {
         insterToFinal(list, i);
@@ -119,6 +130,14 @@ int main()
         insterToFinal(list3, (i + 10) * 3);
     }
 
+    std::cout << "\nBurnt numbers List #1: ";
+    printList(list);
+
+    std::cout << "\nBurnt numbers List #2: ";
+    printList(list2);
+
+    std::cout << "\nBurnt numbers List #3: ";
+    printList(list3);
     // INSERT DATA BY USER;
     bool exit = false;
     int n;
@@ -126,7 +145,7 @@ int main()
     do
     {
         int option;
-        std::cout << "How many numbers do you want to enter: ";
+        std::cout << "\nHow many numbers do you want to enter: ";
         std::cin >> n;
         std::cout << "Which list do you want to put them on?\n"
                   << "1. List #1\n2. List #2\n3. List #3\n4. Exit\nEnter an option: ";
@@ -183,32 +202,71 @@ int main()
     } while (!exit);
 
     // PRINT THE FINAL LIST;
-    std::cout << "LIST #1: " << std::endl;
+    std::cout << "\nLIST #1: " << std::endl;
     printList(list);
 
-    std::cout << "LIST #2: " << std::endl;
+    std::cout << "\nLIST #2: " << std::endl;
     printList(list2);
 
-    std::cout << "LIST #3: " << std::endl;
+    std::cout << "\nLIST #3: " << std::endl;
     printList(list3);
 
     // PRINT THE GREATEST SUM;
     int greatest = greatestSum(list, list2, list3);
     if (greatest == 0)
     {
-        std::cout << "The three lists have the same sum." << std::endl;
+        std::cout << "\nThe three lists have the same sum." << std::endl;
     }
     else if (greatest == 1)
     {
-        std::cout << "The greatest sum is in the list #1." << std::endl;
+        std::cout << "\nThe greatest sum is in the list #1." << std::endl;
     }
     else if (greatest == 2)
     {
-        std::cout << "The greatest sum is in the list #2." << std::endl;
+        std::cout << "\nThe greatest sum is in the list #2." << std::endl;
     }
     else
     {
-        std::cout << "The greatest sum is in the list #3." << std::endl;
+        std::cout << "\nThe greatest sum is in the list #3." << std::endl;
     }
+
+    exit = false;
+    do
+    {
+        int option;
+        std::cout << "\nWhich list do you want to use to search the repeated numbers?\n"
+                  << "1. List #1 with List #2\n2. List #2 with List #3\n3. List #1 with List #3\n4. Exit\nEnter an option: ";
+        std::cin >> option;
+        switch (option)
+        {
+        case 1:
+            std::cout << "\nResult: \n";
+            repeateNumbers(list, list2);
+            break;
+        case 2:
+            std::cout << "\nResult: \n";
+            repeateNumbers(list2, list3);
+            break;
+        case 3:
+            std::cout << "\nResult: \n";
+            repeateNumbers(list, list3);
+            break;
+        case 4:
+            exit = true;
+            break;
+        default:
+            break;
+        }
+
+        std::cout << "\nDo you want search the repeated number again?"
+                    << "\n1. Yes\n2. No\nEnter an option: ";
+        std::cin >> option;
+        if (option == 2)
+        {
+            exit = true;
+        }
+    } while (!exit);
+
+    std::cout << "\nThank you for use the program :) \n";
     return 0;
 }
